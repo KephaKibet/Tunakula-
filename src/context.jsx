d5a6e8e1cd819796a6587183a76454e0d6845e42
Kepha Kibet <kibet.kepha@live.com>
2022-10-01 15:48:21 +0300

    fetch data

---
+++ src/context.jsx
@@ -0,0 +1,35 @@
+import React, {useContext,useEffect} from "react";
+
+const AppContext = React.createContext()
+
+const fetchData = async()=>{
+  try {
+    const response = await fetch('https://randomuser.me/api/')
+    const data = await response.json()
+    console.log(data)
+
+  } catch (error) {
+            console.log(error)
+
+  }
+}
+
+
+const AppProvider = ({ children }) => {
+  useEffect(() => {
+    fetchData()
+  },[])
+  
+  return (
+  < AppContext.Provider value={{name:'John', role:'student'}}>
+    {children}
+    </AppContext.Provider >
+      )
+}
+
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+
+export {AppContext, AppProvider}

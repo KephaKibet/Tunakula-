012ced0cadb861abb4fd448fb6e824563e7f28b9
Kepha Kibet <kibet.kepha@live.com>
2022-10-01 13:59:33 +0300

    contextAPI setup

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import Search from './components/search';
 function App() {
   return (
     <main>
-      <Search />
+      {/* <Search /> */}
       <Meals />
-      <Modal />
-      <Favorites />
+      {/* <Modal /> */}
+      {/* <Favorites /> */}
   </main>
   );
 }

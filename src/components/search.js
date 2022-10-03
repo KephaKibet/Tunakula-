61c77a8547b3b8f937ac20280763f68b6f1e7310
Kepha Kibet <kibet.kepha@live.com>
2022-10-03 21:34:21 +0300

    text state & set search term as text value

--- src/components/search.js
+++ src/components/search.js
@@ -3,11 +3,20 @@ import { useState } from "react";
 import { useGlobalContext } from "../context";
 
 
-const Search  = () => {
+const Search = () => {
   
+  const [text, seTtext] = useState('')
+  
+  const handleChange = (e) => {
+    seTtext(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return <header className="search-container">
-    <form>
-      <input type="text" placeholder='search meal here' className="form-input" />
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder='search meal here' value={text} onChange={handleChange} className="form-input" />
       <button type="submit" className="btn">Search</button>
       <button type="button" className="btn btn-hipster"> Surprise Me!</button>
     </form>
